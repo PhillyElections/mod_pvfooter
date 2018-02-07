bbfdf6e73e1b42d3fc69cf3b8b68bf5461c0b8c0
matthew murphy <mattyhead@gmail.com>
2018-02-07 16:20:09 -0500

    ..

--- mod_pvfooter.php
+++ mod_pvfooter.php
@@ -7,23 +7,19 @@ $language->load('mod_pvfooter', JPATH_ADMIN);
 
 ?>
 <style>
-table.pv-footer {
-	width: 900px;
-	margin-right: auto;
-	margin-left: auto;
-	background-color: #6B9CBD!important;
-}
 
 table.pv-footer td {
-	width: 19%;
+	width: 14.7%;
 }
 </style>
 <table class="pv-footer">
     <tr>
+        <td><a href="/">&nbsp;</td>
         <td><a href="/"><?=JText::_('HOME');?></a></td>
         <td><a href="<?=JText::_('PATH CONTACT US');?>"><?=JText::_('CONTACT US');?></a></td>
         <td><a href="<?=JText::_('PATH VOTERAPP');?>"><?=JText::_('VOTERAPP');?></a></td>
         <td><a href="<?=JText::_('PATH BALLOTBOXAPP');?>"><?=JText::_('BALLOTBOXAPP');?></a></td>
         <td><a href="<?=JText::_('PATH RIGHT TO KNOW POLICY');?>" target="_blank" rel="noreferer"><?=JText::_('RIGHT TO KNOW POLICY');?></a></td>
+        <td><a href="/">&nbsp;</td>
     </tr>
 </table>
