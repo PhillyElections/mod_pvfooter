1b1d59113ef123b819ad0a9200ecfd1d16c10243
matt murphy <mattyhead@gmail.com>
2018-02-09 08:10:43 -0500

    let's do a ul and li's

--- mod_pvfooter.php
+++ mod_pvfooter.php
@@ -8,18 +8,14 @@ $language->load('mod_pvfooter', JPATH_ADMIN);
 ?>
 <style>
 
-table.pv-footer td {
-	width: 8.33%;
 }
 </style>
-<table class="pv-footer">
-    <tr>
-        <td>&nbsp;</td>
-        <td colspan="2"><a href="/"><?=JText::_('HOME');?></a></td>
-        <td colspan="2"><a href="<?=JText::_('PATH CONTACT US');?>"><?=JText::_('CONTACT US');?></a></td>
-        <td colspan="2"><a href="<?=JText::_('PATH VOTERAPP');?>"><?=JText::_('VOTERAPP');?></a></td>
-        <td colspan="2"><a href="<?=JText::_('PATH BALLOTBOXAPP');?>"><?=JText::_('BALLOTBOXAPP');?></a></td>
-        <td colspan="2"><a href="<?=JText::_('PATH RIGHT TO KNOW POLICY');?>" target="_blank" rel="noreferer"><?=JText::_('RIGHT TO KNOW POLICY');?></a></td>
-        <td>&nbsp;</td>
-    </tr>
+<div class="pv-footer">
+    <ul>
+        <li><a href="/"><?=JText::_('HOME');?></a></li>
+        <li><a href="<?=JText::_('PATH CONTACT US');?>"><?=JText::_('CONTACT US');?></a></li>
+        <li><a href="<?=JRoute::_('index.php?option=com_voterapp&tmpl=component');?>"><?=JText::_('VOTERAPP');?></a></li>
+        <li><a href="<?=JRoute::_('index.php?option=com_ballotboxapp');?>"><?=JText::_('BALLOTBOXAPP');?></a></li>
+        <li><a href="<?=JText::_('PATH RIGHT TO KNOW POLICY');?>" target="_blank" rel="noreferer"><?=JText::_('RIGHT TO KNOW POLICY');?></a></li>
+    </ul>
 </table>
